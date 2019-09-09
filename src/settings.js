c1f35336428b2af2dd4476f0817b373fb703136f
Billy Liao <billy_liao@paradise-soft.com.tw>
2019-09-09 16:00:47 +0800

    [Fix] Change Token Storage Type

--- src/settings.js
+++ src/settings.js
@@ -17,5 +17,11 @@ module.exports = {
    * @type {boolean} true | false
    * @description Whether fix the header
    */
-  fixedHeader: true
+  fixedHeader: true,
+
+  /**
+   * @type {string}
+   * @description set TokenKey in LocalStorage
+   */
+  acessTokenKey: 'vue_admin_template_token'
 }
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,16 @@
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
+import Setting from '../settings'
 
-const TokenKey = 'vue_admin_template_token'
+const TokenKey = Setting.acessTokenKey
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return localStorage.getItem(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return localStorage.setItem(TokenKey, token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  return localStorage.removeItem(TokenKey)
 }
