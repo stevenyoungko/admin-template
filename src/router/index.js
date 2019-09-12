7bde792f3b98e54e30e931b37f7a26a52eb7e4c0
Billy Liao <billy_liao@paradise-soft.com.tw>
2019-09-12 13:46:16 +0800

    Feature Router System 註解中文化

--- src/router/index.js
+++ src/router/index.js
@@ -10,19 +10,20 @@ import Layout from '@/layout'
  * Note: sub-menu only appear when route children.length >= 1
  * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
  *
- * hidden: true                   if set true, item will not show in the sidebar(default is false)
- * alwaysShow: true               if set true, will always show the root menu
- *                                if not set alwaysShow, when item has more than one children route,
- *                                it will becomes nested mode, otherwise not show the root menu
- * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
- * name:'router-name'             the name is used on side-bar, side-bar下面的quickMenu, tagViews (must set and unique!!!)
+ * hidden: true                  當設置 true 的時候該路由不會SideBar出現 如401，login等頁面，或者如一些編輯頁面/edit/1
+ * alwaysShow: true              當你一個路由下面的 children 聲明的路由大於1個時，自動會變成嵌套的模式--如組件頁面
+ *                               只有一個時，會將那個子路由當做根路由顯示在側邊欄--如引導頁面
+ *                               若你想不管路由下面的 children 聲明的個數都顯示你的根路由
+ *                               你可以設置 alwaysShow: true，這樣它就會忽略之前定義的規則，一直顯示根路由if set true, will always show the root menu
+ * redirect: noRedirect          當設置 noRedirect 的時候該路由在麵包屑導航中不可被點擊
+ * name:'router-name'            設定路由的名字，一定要填寫不然使用<keep-alive>時會出現各種問題，SideBar下方快速搜尋是需要name (必填)
  * meta : {
-    roles: ['admin','editor']    control the page roles (you can set multiple roles)
-    title: 'title'               the name show in sidebar, breadcrumb, flatMenu, tagViews (must set)
-    icon: 'svg-name'             the icon show in the sidebar
-    breadcrumb: false,           if set false, the item will hidden in breadcrumb(default is true)
+    roles: ['admin','editor']    設置該路由進入的權限，支持多個權限疊加 (目前無使用)
+    title: 'title'               設置該路由在 SideBar,breadcrumb 和 tagViews 中展示的名字 (必填)
+    icon: 'svg-name'             SideBar Icon 的 Svg樣式
+    breadcrumb: false,           如果設置為false，則不會在breadcrumb麵包屑中顯示
     noTagViews: true,            如果設置為true，不會生成tagViews (默認 false)
-    noCache: true,               如果设置为true，则不会被 <keep-alive> 缓存(默认 false)
+    noCache: true,               如果設置為true，則不會被 <keep-alive> 緩存(默認 false) <keep-alive>組件在 @/layout/components/AppMain.vue
     affix: true                  如果設置為true，則會釘選在tagViews上 (默認 false)
   }
  */
