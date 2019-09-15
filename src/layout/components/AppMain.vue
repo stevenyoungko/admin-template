026acdaf7f01a05d2eae634bff84d47224898327
Billy_Liao <billy_liao@paradise-soft.com.tw>
2019-09-15 16:03:08 +0800

    修改提示錯字...

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -26,10 +26,10 @@ export default {
         if (process.env.NODE_ENV !== 'development') return
         if (route.meta && !route.meta.noTagViews) {
           if ((route.meta && route.meta.noCache) && !route.matched[route.matched.length - 1].components.default.name) {
-            console.warn(`[${route.path}]\nLayout下的views 請設置 component name\n如不需要cache 請設置route.meta.noCache`)
+            console.warn(`[${route.path}]\nLayout下的views 請設置 component name\n如不需要cache 請設置router.meta.noCache`)
           }
           if (route.name !== route.matched[route.matched.length - 1].components.default.name) {
-            console.error(`[${route.path}]\nLayout下的views component name 請與 router nameㄧ致,\n如不需要cache 請設置route.meta.noCache`)
+            console.error(`[${route.path}]\nLayout下的views component name 請與 router nameㄧ致,\n如不需要cache 請設置router.meta.noCache`)
           }
         }
       },
