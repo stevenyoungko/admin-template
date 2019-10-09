a34dfdfea84dae7854eadb5a31677a6c55201f11
Billy Liao <billy_liao@paradise-soft.com.tw>
2019-10-09 17:44:30 +0800

    test-1

--- src/settings.js
+++ src/settings.js
@@ -8,7 +8,7 @@ module.exports = {
    * @type {String}
    * @description 變更document.title 預設 PS-admin
    */
-  title: 'PS-admin-test',
+  title: 'PS-admin-test-1',
   /**
    * @type {String}
    * @description 左上角LOGO字串
